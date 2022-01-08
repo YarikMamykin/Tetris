78eb0b38a9be180ec64d4037bfda424b4bf4d726
YarikM <ymamykin@gmail.com>
2022-01-08 19:44:15 +0200

    Add InfoField vue component

---
+++ src/components/fields/info/InfoField.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="infofield">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InfoField',
+}
+</script>
+
+<style scoped>
+div#infofield {
+  background-color: black;
+  display: block;
+}
+</style>
