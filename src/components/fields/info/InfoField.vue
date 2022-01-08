edebbe95085f673b159657220691ad865baba70e
YarikM <ymamykin@gmail.com>
2022-01-08 20:37:08 +0200

    Implement Start button

--- src/components/fields/info/InfoField.vue
+++ src/components/fields/info/InfoField.vue
@@ -3,6 +3,7 @@
     <NextBlock/>
     <Score/>
     <Time/>
+    <Start/>
   </div>
 </template>
 
@@ -10,12 +11,14 @@
 import NextBlock from './next_block/NextBlock.vue';
 import Score from './labels/Score.vue';
 import Time from './labels/Time.vue';
+import Start from './buttons/Start.vue';
 export default {
   name: 'InfoField',
   components: {
     NextBlock,
     Score,
     Time,
+    Start,
   }
 }
 </script>
---
+++ src/components/fields/info/buttons/Start.vue
@@ -0,0 +1,35 @@
+<template>
+  <div style="{ width: 100%; }">
+    <button id="start_button" @click="launch_game">START</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'START',
+  methods: {
+    launch_game() {
+      this.$store.commit('game_controller/start');
+    }
+  }
+}
+</script>
+
+<style scoped>
+button#start_button {
+  background-color: black;
+  color: blueviolet;
+  border: none;
+  width: fit-content;
+  font-size: xx-large;
+  margin-top: 2vh;
+  margin-bottom: 2vh;
+  box-sizing: border-box;
+  padding: 0 5px 0 5px;
+  display: inline-block;
+}
+button#start_button:hover {
+  font-weight: bold;
+  text-shadow: 0 0 10px #fafafa, 0 0 20px blueviolet, 0 0 7px darkorange;
+}
+</style>
