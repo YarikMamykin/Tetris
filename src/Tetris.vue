817f88cb2702856fce0327fc65adcda1f37452db
YarikM <ymamykin@gmail.com>
2022-01-08 18:45:40 +0200

    Add main.js file with app mount and Tetris.vue

---
+++ src/Tetris.vue
@@ -0,0 +1,31 @@
+<template>
+  <Header/>
+  <GameZone/>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import GameZone from './components/GameZone.vue';
+
+export default {
+  name: 'Tetris',
+  components: {
+    Header,
+    GameZone
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: black;
+}
+
+#tetris {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import Tetris from './Tetris.vue'
 import store from './store'
 
-createApp(App).use(store).mount('#app')
+createApp(Tetris).use(store).mount('#tetris')
