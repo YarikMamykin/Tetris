8c9f1c3efb08cf099cd369c11075aa2eb5f85ebe
YarikM <ymamykin@gmail.com>
2022-01-08 18:46:38 +0200

    Add main application components
    1. Header - provides beautiful name of the application ('Tetris')
    2. GameZone - contains all the stuff needed for game i.e.:
    - Field where tetris blocks are placed
    - Field where necessary information and control elements are contained:
    time, score, control buttons, next incoming block, etc.

---
+++ src/components/GameZone.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="gamezone">
+    <GameField/>
+    <InfoField/>
+  </div>
+</template>
+
+<script>
+import GameField from './fields/game/GameField.vue';
+import InfoField from './fields/info/InfoField.vue';
+
+export default {
+  name: 'GameZone',
+  components: {
+    GameField,
+    InfoField
+  }
+}
+</script>
+
+<style scoped>
+div#gamezone {
+  display: grid;
+  grid-template-columns: 40% 30%;
+  grid-column-gap: 10%;
+  margin-left: 20%;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1 id="header">TETRIS</h1>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+h1#header {
+  background-color: black;
+  font-size: xxx-large;
+  background-image: linear-gradient(to right, red, orange, yellow, green, aqua,blue,violet);
+  -webkit-background-clip: text;
+  color: transparent;
+  letter-spacing: 30px;
+  display: inline-block;
+  margin-top: 0;
+}
+</style>
